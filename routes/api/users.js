a6f3bdc8c8a42803393e5689e7ec648df62b6a04
Nikolai <nvanbaak@gmail.com>
2020-11-12 14:52:48 -0800

    user routes complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,16 +6,32 @@ const db = require("../../models");
 router.route("/users")
     .get((req, res) => {
         db.User.find(req.query)
-            .sort({ id: 1 })
+            .sort({ _id: 1 })
             .then(results => res.json(results))
             .catch(err => res.status(422).json(err));
     }) // end of get()
     .post((req, res) => {
         db.User.find(req.query)
-            .sort({ id: 1 })
+            .sort({ _id: 1 })
             .then(result => res.json(result))
             .catch(err => res.status(422).json(err));
     }); // end of post()
 
 // Request with an id
-// router.route("./:id")
+router.route("./:id")
+    .get((req, res) => {
+        db.User.findById(req.params.id)
+            .then(result => res.json(result))
+            .catch(err => res.status(422).json(err));
+    }) // end of get()
+    .put((req, res) => {
+        db.User.findOneandUpdate( {_id: req.params.id}, req.body)
+            .then( result => res.json(result) )
+            .catch(err => res.status(422).json(err));
+    }) // end of put()
+    .delete((req, res) => {
+        db.User.findById(req.params.id)
+            .then(result => result.remove())
+            .then(result => res.json(result))
+            .catch(err => res.status(422).json(err));
+    }) // end of delete()
