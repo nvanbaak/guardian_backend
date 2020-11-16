531cee03a5bf7a361800b8f87251efc930674118
Nikolai <nvanbaak@gmail.com>
2020-11-16 14:03:12 -0800

    fixed routing

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,9 +6,6 @@ const bcrypt = require("bcrypt");
 // Request without an id
 router.route("/users")
     .get((req, res) => {
-
-        console.log(db);
-
         db.User.find(req.query)
             .sort({ _id: 1 })
             .then(results => res.json(results))
@@ -30,14 +27,14 @@ router.route("/users")
     }); // end of post()
 
 // Request with an id
-router.route("./users/:id")
+router.route("/users/:id")
     .get((req, res) => {
         db.User.findById(req.params.id)
             .then(result => res.json(result))
             .catch(err => res.status(422).json(err));
     }) // end of get()
     .put((req, res) => {
-        db.User.findOneandUpdate( {_id: req.params.id}, req.body)
+        db.User.findOneAndUpdate( {_id: req.params.id}, req.body)
             .then( result => res.json(result) )
             .catch(err => res.status(422).json(err));
     }) // end of put()
