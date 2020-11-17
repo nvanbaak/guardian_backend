6e7d07b6e9a40bef642a10a1e7ffbcd8a6b2f135
Nikolai <nvanbaak@gmail.com>
2020-11-17 12:15:13 -0800

    added default message to slash route

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require("./api");
 // API
 router.use("/api", apiRoutes);
 router.route("/").get((req, res) => {
-    res.send("../index.html");
+    res.send("Backend is up!");
 })
 
 module.exports = router;
