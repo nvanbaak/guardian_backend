77b28d9f54a45a625ab826ae2e9f6d2127b2f24b
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-17 10:25:34 -0800

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 // Dependencies
 const router = require("express").Router();
 const db = require("../../models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("../../config/auth.config");
 
 // Request without an id
 router.route("/users")
@@ -11,7 +14,10 @@ router.route("/users")
             .catch(err => res.status(422).json(err));
     }) // end of get()
     .post((req, res) => {
-        db.User.create(req.body)
+        db.User.create({
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10),null)
+        })
             .then(result => res.json(result))
             .catch(err => res.status(422).json(err));
     }); // end of post()
@@ -34,5 +40,46 @@ router.route("./users/:id")
             .then(result => res.json(result))
             .catch(err => res.status(422).json(err));
     }) // end of delete()
+
+
+// Signin post request
+router.route("/users/signin")
+    .post((req,res) => {
+        db.User.findOne({
+            email: req.body.email
+        })
+            .exec((err, user) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                if (!user) {
+                    return res.status(404).send({ message: "User not found!" });
+                }
+
+                var passwordIsValid = bcrypt.compareSync(
+                    req.body.password,
+                    user.password
+                );
+
+                if (!passwordIsValid) {
+                    return res.status(401).send({
+                        accessToken: null,
+                        message: "Invalid password!"
+                    });
+                }
+
+                var token = jwt.sign({ id: user.id }, config.secret, {
+                    expiresIn: 86400 //24 hours
+                });
+
+                res.status(200).json({
+                    id: user._id,
+                    email: user.email,
+                    accessToken: token
+                });
+            });
+    });
 
 module.exports = router;
